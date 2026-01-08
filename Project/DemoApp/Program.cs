faba50c2c5d203a067ac186d753bbfff1fd6f3fa
Arpanrai0007 <arpankumarrai007@gmail.com>
2026-01-08 19:22:35 +0530

    Update Program.cs

--- Project/DemoApp/Program.cs
+++ Project/DemoApp/Program.cs
@@ -1,1 +1,14 @@
+using System;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from .NET application!");
+            Console.WriteLine($"Current Time: {DateTime.Now}");
+        }
+    }
+}
 
